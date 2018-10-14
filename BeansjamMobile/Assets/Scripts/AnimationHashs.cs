71dfe8ad1457d3a2ff0c115bb404ef8d32980754
Joni24 <droggelbecher@hotmail.de>
2018-10-14 04:18:05 +0200

    geisterbahn + animation eingebaut

---
+++ BeansjamMobile/Assets/Scripts/AnimationHashs.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class AnimationHashs : MonoBehaviour {
+    public static readonly int GEISTERBAHN_GHOSTS = Animator.StringToHash("Trigger_Ghosts");
+    public static readonly int GEISTERBAHN_MONSTER = Animator.StringToHash("Trigger_Monster");
+}
